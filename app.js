e4aff712624fa89731f287a2461c5edeefd78b13
thallesborges <dev.thallesborges@gmail.com>
2025-09-11 13:43:35 -0300

    implementa função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+    let input_nome = document.querySelector('input');
+    let nome = input_nome.value.trim(); // remove espaços extras: '    Thalles  ' = 'Thalles'
+
+    if (nome === "") {
+        alert('Por favor, insira um nome.');
+    } else {
+        nomes.push(nome);
+    }
+
+    console.log(nomes);
+
+    // Limpa o campo de input
+    input_nome.value = '';
+}
