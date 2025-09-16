f713165b057b2095b43e81c0d2ff41f4d9cef1a7
thallesborges <dev.thallesborges@gmail.com>
2025-09-16 15:13:30 -0300

    aplica validação nos nomes para que não haja nomes repetidos & adiciona opção de inserir o nome  com a tecla enter

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 let amigos = [];
 let listaAmigos = document.getElementById('listaAmigos');
 let resultado = document.getElementById('resultado');
+let input_nome = document.querySelector('input');
+
+// Enviar o input apertando enter
+input_nome.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        adicionarAmigo();
+    }
+});
+
+function formatarNome(nome) {
+    return nome
+        .toLowerCase()
+        .split(" ")
+        .filter(p => p !== "")
+        .map(p => p.charAt(0).toUpperCase() + p.slice(1))
+        .join(" ");
+}
 
 function adicionarAmigo() {
-    let input_nome = document.querySelector('input');
     let nome = input_nome.value.trim(); // remove espaços extras: '    Thalles  ' = 'Thalles'
+    
     if (nome === "") {
         alert('Por favor, insira um nome.');
-    } else {
-        amigos.push(nome);
+        return;
     }
 
-    listar_amigos();
+    // Normalizar o nome para que não haja nomes iguais: 'thalles' e 'Thalles', 'João' e 'Joao'
+    let nomeNormalizado = nome
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, "");
+
+    let jaExiste = amigos.some(amigo => 
+        amigo
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "") === nomeNormalizado
+    );
+
+    if (jaExiste) {
+        alert('Esse nome já foi adicionado!');
+    } else {
+        let nomeFormatado = formatarNome(nome)
+
+        amigos.push(nomeFormatado);
+        console.log(amigos);
+    }
+
+    listarAmigos();
     input_nome.value = '';
 }
 
-function listar_amigos() {
+function listarAmigos() {
     
     listaAmigos.innerHTML = '';
 
